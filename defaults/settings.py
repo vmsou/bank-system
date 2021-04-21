6f1b221086286ae0b9fefffb2bbcbd7e77305c91
vmsou <viniciusmarcs@icloud.com>
2021-04-20 21:10:30 -0300

    Import Fix

--- defaults/settings.py
+++
@@ -1,5 +0,0 @@
-class Settings:
-
-    def __init__(self):
-        self.db_file = "users.sqlite"
-        self.db_name = "users"
---
+++ sistema_bancario/defaults/settings.py
@@ -0,0 +1,5 @@
+class Settings:
+
+    def __init__(self):
+        self.db_file = "users.sqlite"
+        self.db_name = "users"
