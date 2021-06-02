d97811438928cbdc6570715b8e92664c65588722
leonardofalango <81644264+leonardofalango@users.noreply.github.com>
2021-06-02 18:08:50 -0300

    Update validation.py

--- sistema_bancario/defaults/validation.py
+++ sistema_bancario/defaults/validation.py
@@ -4,10 +4,15 @@ Esse seção permite verificar as entradas do úsuario ou do gerente.
 Seguinda uma base de críterios retorna ao sistema se as informações
 inseridas são validas
 """
+import re
 
 
 def check_name(name):
     """TODO: Verificar se nome possui nome e sobrenome"""
+    nomeSimples = re.search(r" ", name) is None
+    sobrenome = not(nomeSimples)
+    return sobrenome
+    
     ...
 
 
@@ -30,14 +35,57 @@ def check_address(address):
 
 def check_phone(phone_number):
     """TODO: Usar regex para identificar validade do telefone"""
+    
+   phoneOK = False
+    if len(phone_number) <10 and len(phone_number) >= 8:
+      phoneOK= True
+    else:
+      phoneOK= False
+    return phoneOK
     ...
 
 
-def check_password(password):
+
+ def check_password(password):
     """
     -> Para facilitar usar módulo string
-    TODO: Senha maior que 8 caracteres
-    TODO: precisando de letras maisculas e minusculas
-    TODO: Recusar acentos e/ou caracteres especiais
+        8 caracteres ou mais
+        1 numero
+        1 letra maiuscula
+        1 letra minuscula
+        Nao permitido caracteres especiais
     """
+
+    # calculating the length
+    tamanhoErro = len(password) < 8
+
+    # searching for digits
+    numeroErro = re.search(r"\d", password) is None
+
+    # searching for uppercase
+    maiusculaErro = re.search(r"[A-Z]", password) is None
+
+    # searching for lowercase
+    minusculaErro = re.search(r"[a-z]", password) is None
+
+    # searching for symbols
+    simboloErro = re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) is None
+
+
+    # overall result
+    password_ok = not (tamanhoErro or numeroErro or maiusculaErro or minusculaErro or not(simboloErro))
+
+    # ERROS:
+    if tamanhoErro:
+      print('\033[0;31mA senha deve ter pelo menos 8 caracteres\033[m')
+    if numeroErro:
+      print('\033[0;31mA senha deve ter no minino 1 numero\033[m')
+    if maiusculaErro:
+      print('\033[0;31mA senha deve ter ao menos 1 letra maisucula\033[m')
+    if minusculaErro:
+      print('\033[0;31mA senha deve ter ao menos 1 letra minuscula\033[m')
+    if not(simboloErro):
+      print('\033[0;31mCaractere nao permitido\033[m')
+    if (tamanhoErro or numeroErro or maiusculaErro or minusculaErro or not(simboloErro)):
+      return password_ok
     ...
